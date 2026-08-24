50ece7bd7273f389e7a763b9b6d3a16ec695795e
Hélder Marques <helder.marques@kuehne-nagel.com>
2026-08-24 17:03:37 +0100

    Add variables and outputs

--- 000_getting_started/main.tf
+++ 000_getting_started/main.tf
@@ -23,12 +23,24 @@ provider "aws" {
 #   owners = ["099720109477"] # Canonical
 # }
 
+variable "instance_type" {
+  type = string
+}
+
+locals {
+  project_name = "MyProject"
+}
+
 resource "aws_instance" "my_server" {
   # ami= data.aws_ami.ubuntu.id
   ami           = "ami-0236922087fa98b6e" # Replace with a valid AMI ID
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
-    Name = "MyServer"
+    Name = "MyServer-${local.project_name}"
   }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.my_server.public_ip
 }
